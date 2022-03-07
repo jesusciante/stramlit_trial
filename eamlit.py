45f227e45b6f2d6ce3aaa2d20196f18e790822ae
jesusciante <oguzhanusta.10@gmail.com>
2022-03-07 19:20:49 +0300

    First Commit

---
+++ eamlit.py
@@ -0,0 +1,9 @@
+from cProfile import label
+import streamlit as st
+import plotly.express as ex
+
+st.title('asda')
+
+st.sidebar.subheader('sadsd')
+
+uploaded_file = st.sidebar.file_uploader(label="sdasd",type=['xlsx'])
