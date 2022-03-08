59fdc1de0bd32c1f65f713027dae0e5ffb94109e
jesusciante <oguzhanusta.10@gmail.com>
2022-03-08 19:39:53 +0300

    master

--- eamlit.py
+++ eamlit.py
@@ -13,32 +13,18 @@ def sql_executor(raw_code):
 	data = c.fetchall()
 	return data 
 
-
-
-
-def main():
-	st.title("SQLPlayground")
-
-
 st.subheader("HomePage")
 
 
-st.form(key='query_form')
-raw_code = st.text_area("SQL Code Here")
-
+st.form(key='Mark Number Search')
+raw_code = st.text_area("Mark Number Here")
 
 st.code(raw_code)
 
-query_results = sql_executor(raw_code)
+query_results = sql_executor("select*from SteelFab where MARKNUMBER like'" + raw_code + "%'")
 
 
 st.expander("Pretty Table")
 query_df = pd.DataFrame(query_results)
 st.dataframe(query_df)
 
-
-
-
-
-if __name__ == '__main__':
-	main()
